067773edbba7bf19659214b52a17a6e16fc4fa76
anupama <anupama_avula@yahoo.com>
2020-05-26 21:21:06 -0400

    first commit

---
+++ greeting.js
@@ -0,0 +1,103 @@
+//Person class
+var Person = /** @class */ (function () {
+    //Constuctor
+    function Person(firstName, lastName, age, phoneNum, state, zipCode, occupation, hourlyWage, hours) {
+        this.certification = [];
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.phoneNum = phoneNum;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.occupation = occupation;
+        this.hourlyWage = hourlyWage;
+        this.hours = hours;
+    }
+    //Getters and Setters
+    Person.prototype.getFirstName = function () {
+        return this.firstName;
+    };
+    Person.prototype.setFirstName = function (firstName) {
+        this.firstName = firstName;
+    };
+    Person.prototype.getLastName = function () {
+        return this.lastName;
+    };
+    Person.prototype.setLastName = function (lastName) {
+        this.lastName = lastName;
+    };
+    Person.prototype.getAge = function () {
+        return this.age;
+    };
+    Person.prototype.setAge = function (age) {
+        this.age = age;
+    };
+    Person.prototype.getPhonenum = function () {
+        return this.phoneNum;
+    };
+    Person.prototype.setPhoneNum = function (phoneNum) {
+        this.phoneNum = phoneNum;
+    };
+    Person.prototype.getState = function () {
+        return this.state;
+    };
+    Person.prototype.setState = function (state) {
+        this.state = state;
+    };
+    Person.prototype.getZipCode = function () {
+        return this.zipCode;
+    };
+    Person.prototype.setZipCode = function (zipCode) {
+        this.zipCode = zipCode;
+    };
+    Person.prototype.getOccupation = function () {
+        return this.occupation;
+    };
+    Person.prototype.setOccupation = function (occupation) {
+        this.occupation = occupation;
+    };
+    //Methods
+    Person.prototype.fullName = function () {
+        return "My name is " + this.firstName + " " + this.lastName;
+    };
+    Person.prototype.nameAndNum = function () {
+        return ("My name is   " + this.firstName + "  " + this.lastName + " and my phone number is "
+            + this.phoneNum);
+    };
+    Person.prototype.nameAndAddress = function () {
+        return ("My name is  " + this.firstName + "  " + this.lastName + "and I live in " + this.state
+            + " with Zipcode " + this.zipCode);
+    };
+    Person.prototype.wageRate = function () {
+        if (this.hours) {
+            this.hours = this.hours;
+        }
+        else {
+            this.hours = 40;
+        }
+        var weeklyWage = this.hourlyWage * this.hours;
+        return ("My name is " + this.firstName + " and I my wage is $" + weeklyWage);
+    };
+    Person.prototype.addCerts = function () {
+        var _a;
+        var degrees = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            degrees[_i] = arguments[_i];
+        }
+        (_a = this.certification).push.apply(_a, degrees);
+        return "My Certifications are:" + this.certification.join(",");
+    };
+    Person.prototype.createPerson = function (person) {
+        return person;
+    };
+    return Person;
+}());
+var Anu = new Person("Anu", "Avula", 35, 9898989898, "NC", 28270, "Developer");
+var Anil = new Person("Anil", "Kak", 23, 1234567890, "NC", 28270, "Developer");
+var Tarini = new Person("Tarini", "Kak", 11, 4444567890, "NC", 28270, "Student");
+var John = new Person("John", "Man", 28, 123456789, "NC", 28226, "HandyMan", 20);
+document.getElementById("personName").innerHTML = Anu.fullName();
+document.getElementById("personNameNum").innerHTML = Anil.nameAndNum();
+document.getElementById("personNameAddress").innerHTML = Tarini.nameAndAddress();
+document.getElementById("personWage").innerHTML = John.wageRate();
+document.getElementById("personCert").innerHTML = John.addCerts("MBA", "CSM", "MCSD");
---
+++ greeting.ts
@@ -0,0 +1,161 @@
+// function sayHello(person: string): string {
+//     return "Hello, " + person;
+//     }
+//     var user = "Super Student";
+//     document.getElementById("para").innerHTML = sayHello(user);
+    //Interface
+      interface iPerson{
+        firstName : string;
+        lastName  : string;
+        age : string;
+        phoneNum : number;
+        state : string;
+        zipCode : number;
+        occupation : string;
+        hourlyWage : number;
+        certification :string[];
+
+      }
+
+      //Person class
+
+    class Person {
+        private firstName: string;
+        private lastName: string;
+        private age: number;
+        private phoneNum: number;
+        private state: string;
+        private zipCode: number;
+        private occupation: string;
+        private hourlyWage?: number;
+        private hours?: number;
+        private certification: string[] = [];
+
+        //Constuctor
+        constructor(firstName: string,lastName: string,age: number,phoneNum: number,state: string,
+          zipCode: number,occupation: string, hourlyWage?: number, hours?: number,) 
+        {
+          this.firstName = firstName;
+          this.lastName = lastName;
+          this.age = age;
+          this.phoneNum = phoneNum;
+          this.state = state;
+          this.zipCode = zipCode;
+          this.occupation = occupation;
+          this.hourlyWage = hourlyWage;
+          this.hours = hours;
+          
+        }
+
+        //Getters and Setters
+
+        getFirstName() : string{
+          return this.firstName;
+        }
+        setFirstName(firstName: string): void {
+          this.firstName = firstName;
+        }
+        getLastName() : string{
+          return this.lastName;
+        }
+        setLastName(lastName: string): void {
+          this.lastName = lastName;
+        }
+        getAge() : number{
+          return this.age;
+        }
+        setAge(age: number): void {
+          this.age = age;
+        }
+        getPhonenum() : number{
+          return this.phoneNum;
+        }
+        setPhoneNum(phoneNum: number): void {
+          this.phoneNum = phoneNum;
+        }
+        getState() : string{
+          return this.state;
+        }
+        setState(state: string): void {
+          this.state = state;
+        }
+        getZipCode() : number{
+          return this.zipCode;
+        }
+        setZipCode(zipCode: number): void {
+          this.zipCode = zipCode;
+        }
+        getOccupation() : string{
+          return this.occupation;
+        }
+        setOccupation(occupation: string): void {
+          this.occupation = occupation;
+        }
+      
+
+        //Methods
+        fullName(): string {
+          return "My name is " + this.firstName + " " + this.lastName;
+        }
+        nameAndNum(): string {
+          return ("My name is   " +this.firstName +"  " +this.lastName +" and my phone number is " 
+                 +this.phoneNum);
+        }
+        nameAndAddress(): string {
+          return ("My name is  " +this.firstName + "  " +this.lastName +"and I live in " +this.state
+            +" with Zipcode " +this.zipCode );
+        }
+        wageRate(): any 
+        {
+          if (this.hours) {
+            this.hours = this.hours;
+          } else {
+            this.hours = 40;
+          }
+          let weeklyWage = this.hourlyWage * this.hours;
+          return (
+            "My name is " + this.firstName + " and I my wage is $" + weeklyWage );
+        }
+        addCerts(...degrees: string[]): string {
+          this.certification.push(...degrees);
+          return "My Certifications are:"+this.certification.join(",");
+        }
+        createPerson(person?: iPerson):{firstName:string;lastName:string}{
+          return person;
+        }
+
+      }
+      let Anu = new Person("Anu", "Avula", 35, 9898989898, "NC", 28270, "Developer");
+      let Anil = new Person("Anil","Kak", 23, 1234567890, "NC", 28270, "Developer");
+      let Tarini = new Person("Tarini","Kak",11, 4444567890, "NC",28270,"Student");
+      let John = new Person("John", "Man", 28, 123456789,"NC", 28226,"HandyMan",20);
+      
+      document.getElementById("personName").innerHTML = Anu.fullName();
+      document.getElementById("personNameNum").innerHTML = Anil.nameAndNum();
+      document.getElementById("personNameAddress").innerHTML = Tarini.nameAndAddress();
+      document.getElementById("personWage").innerHTML = John.wageRate();
+      document.getElementById("personCert").innerHTML = John.addCerts("MBA","CSM","MCSD");
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
